1c9910e28b6d7cc5bbeb99c6a2ccde6f04acd84c
Julia <gullya@yandex.ru>
2022-09-24 17:23:58 +0400

    part1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { CreateContainer } from "./components/CreateContainer";
+import {AnimatePresence} from "framer-motion"
+
+import {Header} from "./components/Header"
+import { MainContainer } from "./components/MainContainer";
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-    <h1>Restaurant menu</h1>
-    </div>
+<AnimatePresence>
+     <div className="w-screen h-auto flex flex-col bg-primary">
+     <Header />
+
+     <main className="mt-24 p-8 w-full ">
+     <Routes>
+            <Route path="/*" element={<MainContainer />} />
+            <Route path="/createItem" element={<CreateContainer />} />
+          </Routes>
+      </main>
+     </div>
+     </AnimatePresence>
+   
   );
 }
 
---
+++ src/components/CreateContainer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CreateContainer = () => {
+  return (
+    <div>Create item</div>
+  )
+}
