4d3d32035c409e4ddf4574a6e6eaad846232f1c9
armanapresyan <armanapresyan2020@gmail.com>
2022-09-27 23:56:42 +0400

    add section

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -3,8 +3,10 @@ import HomeConteiner from "./HomeConteiner";
 import { motion } from "framer-motion";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import RowContainer from "./RowContainer";
+import { useStateValue } from "../context/StateProvider";
 
 const MainContainer = () => {
+  const [{foodItems}, dispatch] = useStateValue("")
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center">
       <HomeConteiner />
@@ -34,8 +36,8 @@ const MainContainer = () => {
             </motion.div>
           </div>
         </div>
-
-        <RowContainer flag={true}/>
+  
+        <RowContainer flag={true} data={foodItems}/>
       </section>
     </div>
   );
