1dc5cdb0ff859ba2fe022e75298b8db9d5ce90e5
Hasmiktx <hasmiktx@gmail.com>
2022-10-07 22:48:34 +0400

    added actons in sliceCart

--- src/redux/sliceCart.js
+++ src/redux/sliceCart.js
@@ -18,8 +18,35 @@ const sliceCart = createSlice({
     setCartItems(state, action) {
       state.cartItems = action.payload;
     },
+    addCount(state, action) {
+      state.cartItems = state.cartItems.map((item) => {
+        if (item.id === action.payload) {
+          item.qty += 1;
+        }
+        return item;
+      });
+    },
+    removeCount(state, action) {
+      state.cartItems = state.cartItems.map((item) => {
+        if (item.id === action.payload) {
+          item.qty -= 1;
+        }
+        return item;
+      });
+    },
+    filterCart(state, action) {
+      state.cartItems = state.cartItems.filter(
+        (item) => item.id !== action.payload
+      );
+    },
   },
 });
 
-export const { changeCartShow, setCartItems } = sliceCart.actions;
+export const {
+  changeCartShow,
+  setCartItems,
+  addCount,
+  filterCart,
+  removeCount,
+} = sliceCart.actions;
 export default sliceCart.reducer;
